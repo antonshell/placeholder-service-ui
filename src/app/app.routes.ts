dac5175e82b80ad948c81b8a07b8c3f40ea854cf
Anton Shelyganov <antonshell@yandex.ru>
2024-04-12 20:33:12 +0400

    Created placeholder and bootstrap components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {AboutComponent} from "./components/about/about.component";
+import {PlaceholderComponent} from "./components/placeholder/placeholder.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PlaceholderComponent },
+  { path: 'about', component: AboutComponent },
+];
